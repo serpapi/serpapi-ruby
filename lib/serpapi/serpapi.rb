e157109475cb8babc294950003249e824955d1d5
Victor Benarbia <victor@serpapi.com>
2022-07-19 14:54:07 -0500

    improve documentation

--- lib/serpapi/serpapi.rb
+++ lib/serpapi/serpapi.rb
@@ -1,37 +1,39 @@
 # SerpApi module provides serpapi.com client
 #
 module SerpApi
+
   # Client for SerpApi.com
   #
   class Client
     VERSION = '1.0.0'.freeze
     BACKEND = 'serpapi.com'.freeze
 
-    # search parameter
-    attr_accessor :parameter
-    # HTTP read timeout
+    # HTTP read timeout in seconds (default: 60s)
     attr_accessor :read_timeout
 
-    # constructor
+    # Constructor
     #
     # Example:
     # ```ruby
     # require 'serpapi'
-    # client = SerpApi::Client.new(api_key: "secure API key", engine: "google")
+    # client = SerpApi::Client.new(api_key: "secure API key", engine: "google", timeout: 30)
     # result = client.search(q: "coffee")
     # ```
-    # The parameter hash should contains the following:
-    #  engine [String] search enginge selected
+    # The parameter hash should contains the following optional field:
     #  api_key [String] user secret API key
-    #  read_timeout [Integer] HTTP read max timeout
+    #  engine [String] search enginge selected
+    #  timeout [Integer] HTTP read max timeout in seconds (default: 60s)
     #
-    # @param [Hash] parameter default
+    # key can be either a symbol or a string.
+    #
+    # @param [Hash] parameter default for the search
     def initialize(parameter = {})
-      # set default paramter
-      @default = parameter || {}
-
       # set default read timeout
-      @read_timeout = parameter[:read_timeout] || 100
+      @read_timeout = parameter[:timeout] || parameter['timeout'] || 60
+
+      # set default paramter
+      @default = parameter.clone || {}
+      @default.freeze
     end
 
     # perform a search using serpapi.com
@@ -44,14 +46,14 @@ module SerpApi
     #                 thus, most of the compute power is not pass on the client.
     #
     def search(parameter = {})
-      start('/search', :json, parameter)
+      get('/search', :json, parameter)
     end
 
     # html search
     #
     # @return [String] raw html search results directly from the search engine
     def html(parameter = {})
-      start('/search', :html, parameter)
+      get('/search', :html, parameter)
     end
 
     # Get location using Location API
@@ -61,25 +63,24 @@ module SerpApi
     #
     # example: spec/serpapi/location_api_spec.rb
     def location(parameter = {})
-      start('/locations.json', :json, parameter)
+      get('/locations.json', :json, parameter)
     end
 
     # Retrieve search result from the Search Archive API
     #
     # @param [String|Integer] search_id is returned
     # @param [Symbol] format :json or :html (default: json, optional)
-    # @return [String|Hash] raw html or
-    #
+    # @return [String|Hash] raw html or JSON / Hash
     def search_archive(search_id, format = :json)
       raise SerpApiException, 'format must be json or html' unless %i[json html].include?(format)
-      start("/searches/#{search_id}.#{format}", format, nil)
+      get("/searches/#{search_id}.#{format}", format, nil)
     end
 
     # Get account information using Account API
-    # @param [String] optional api_key secret key
+    # @param [String] api_key secret key
     def account(api_key = nil)
       parameter = (api_key.nil? ? {} : { api_key: api_key })
-      start('/account', :json, parameter)
+      get('/account', :json, parameter)
     end
 
     # @return [String] default search engine
@@ -87,41 +88,54 @@ module SerpApi
       @default['engine'] || @default[:engine]
     end
 
-    # @return [Hash] default parameter
-    def parameter
+    # @return [Hash] parameter default as defined in the constructor
+    def default_parameter
       @default
     end
 
-    # @return [String] api_key default search key
+    # @return [String] api_key user secret api_key as supplied in the constructor parameter[:api_key]
     def api_key
       @default['api_key'] || @default[:api_key]
     end
 
-    private
-
-    # build url
-    #
-    def build_url(path, parameter)
-      # force default
-      param = (@default || {}).merge(parameter || {})
-
-      # set ruby client
-      param[:source] = 'ruby'
-
-      # delete empty key/value
-      param.delete_if { |_, value| value.nil? }
-
-      # HTTP parameter encoding
-      q = URI.encode_www_form(param)
-
-      # return URL
-      URI::HTTPS.build(host: BACKEND, path: path, query: q)
+    # @return [Integer] timeout in millisecond HTTP read
+    def timeout
+      @read_timeout
     end
 
-    # start HTTP query
-    def start(path, decoder = :json, parameter = {})
-      url = build_url(path, parameter)
-      payload = get(url)
+    private
+
+    # build url from user parameter 
+    #
+    # @param [String] endpoint HTTP service uri
+    # @param [Hash] parameter custom search inputs
+    # @return formatted HTTPS URL
+    def build_url(endpoint, parameter)
+      # force default
+      query = (@default || {}).merge(parameter || {})
+
+      # set ruby client
+      query[:source] = 'ruby'
+
+      # delete empty key/value
+      query.delete_if { |_, value| value.nil? }
+
+      # HTTP parameter encoding
+      encoded_query = URI.encode_www_form(query)
+
+      # return URL
+      URI::HTTPS.build(host: BACKEND, path: endpoint, query: encoded_query)
+    end
+
+    # get HTTP query
+    #
+    # @param [String] endpoint HTTP service uri
+    # @param [:json|:html] decoder type
+    # @param [Hash] parameter custom search inputs
+    # @return decoded payload as JSON / Hash or String 
+    def get(endpoint, decoder = :json, parameter = {})
+      url = build_url(endpoint, parameter)
+      payload = URI(url).open(read_timeout: @read_timeout).read
       return decode(payload, decoder)
     rescue OpenURI::HTTPError => e
       data = JSON.parse(e.io.read)
@@ -133,12 +147,11 @@ module SerpApi
       raise SerpApiException, "fail: get url: #{url} caused by: #{e}"
     end
 
-    # get URL content
-    def get(url)
-      URI(url).open(read_timeout: read_timeout).read
-    end
-
     # decode payload using json or html
+    #
+    # @param [String] payload to decode
+    # @param [:json|:html] decoder type
+    # @return decoded payload as JSON / Hash or String 
     def decode(payload, decoder)
       case decoder
       when :json
