96e3e21d2bbf3ad0462090dcefa612b4164dc1e2
ilyazub <ilya@serpapi.com>
2023-12-22 17:46:24 +0100

    Fix code examples syntax highlighting in YARD documentation
    Co-authored-by: Thomas Hurst <thomas@serpapi.com>

--- spec/serpapi/example_search_google_local_services_spec.rb
+++ spec/serpapi/example_search_google_local_services_spec.rb
@@ -1,11 +1,10 @@
 describe 'example: google_local_services search' do
-  xit 'prints local_ads' do
+  it 'prints local_ads' do
     client = SerpApi::Client.new(api_key: ENV['API_KEY'], engine: 'google_local_services')
     results = client.search({
-      'q': 'Electrician',
-      'place_id': 'ChIJOwg_06VPwokRYv534QaPC8g'
+      'q': 'electrician',
+      'data_cid': '14414772292044717666'
     })
-    pp results
     expect(results[:local_ads]).not_to be_nil
     # pp results[:local_ads]
     # ENV['API_KEY'] captures the secret user API available from http://serpapi.com
